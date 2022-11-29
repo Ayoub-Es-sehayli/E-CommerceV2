6a84d77def0e01dbfbbfb8ae57efb7895f25b943
Es-sehayli Ayoub <ayoub1essehayli@outlook.com>
2022-11-29 07:07:25 +0100

    Forwarding button ref to base button tag

--- src/components/buttons/button.tsx
+++ src/components/buttons/button.tsx
@@ -4,21 +4,22 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   icon?: string;
   minWidth?: string;
 }
-const Button: React.FC<ButtonProps> = ({
-  children,
-  icon,
-  minWidth,
-  ...props
-}) => {
-  let className =
-    "flex gap-2 justify-center items-center px-6 py-3 rounded-lg text-lg font-semibold whitespace-nowrap ";
-  if (minWidth) className += minWidth + " ";
-  else className += " w-min ";
-  return (
-    <button {...props} className={className + props.className}>
-      {icon ? <i className={icon}></i> : null}
-      <span>{children}</span>
-    </button>
-  );
-};
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ children, icon, minWidth, ...props }, forwardedRef) => {
+    let className =
+      "flex gap-2 justify-center items-center px-6 py-3 rounded-lg text-lg font-semibold whitespace-nowrap ";
+    if (minWidth) className += minWidth + " ";
+    else className += " w-min ";
+    return (
+      <button
+        {...props}
+        className={className + props.className}
+        ref={forwardedRef}
+      >
+        {icon ? <i className={icon}></i> : null}
+        <span>{children}</span>
+      </button>
+    );
+  }
+);
 export default Button;
