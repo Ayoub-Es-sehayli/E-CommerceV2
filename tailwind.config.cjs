cab3d4eaa42140ca35d855dfec803354620b464a
Es-sehayli Ayoub <ayoub1essehayli@outlook.com>
2022-12-02 02:09:51 +0100

    font setup for next & ladle

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+
+    fontFamily: {
+      sans: ["Montserrat", "sans-serif"],
+    },
   },
   plugins: [],
 };
