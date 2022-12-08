9b91f7025a1419e6c3fef3ad8ef8f611de194a76
Es-sehayli Ayoub <ayoub1essehayli@outlook.com>
2022-12-08 16:40:21 +0100

    Implemented a spinner as loading ui element

--- src/components/notification/index.ts
+++ src/components/notification/index.ts
@@ -1,1 +1,3 @@
+import Spinner from "./spinner";
 export { Toast, ToastProvider, useToast } from "./toast";
+export { Spinner };
--- src/stories/notifications.stories.tsx
+++ src/stories/notifications.stories.tsx
@@ -1,7 +1,8 @@
 import "bootstrap-icons/font/bootstrap-icons.css";
+import { useCallback, useState } from "react";
 import "tailwindcss/tailwind.css";
 import Button from "../components/buttons";
-import { ToastProvider, useToast } from "../components/notification";
+import { Spinner, ToastProvider, useToast } from "../components/notification";
 
 export const NotificationSystem = () => {
   const showToast = useToast((state) => state.showToast);
@@ -13,5 +14,23 @@ export const NotificationSystem = () => {
         Do Something
       </Button>
     </ToastProvider>
+  );
+};
+
+export const LoadingPlaceholder = () => {
+  const [isLoading, setLoading] = useState(false);
+  const toggleLoading = useCallback(() => {
+    setLoading(!isLoading);
+  }, [isLoading]);
+  return (
+    <div className="flex items-center justify-center gap-4">
+      <Button
+        onClick={toggleLoading}
+        className="bg-rose-700 py-1 px-3 text-white"
+      >
+        <Spinner isLoading={isLoading} />
+        <span>Click me</span>
+      </Button>
+    </div>
   );
 };
